34f9612935fc4f86ece03c99683094945ffe19a4
marpme <7430964+marpme@users.noreply.github.com>
2022-12-09 20:17:45 +0100

    chore: add prettier checks and ci runner

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
@@ -8,8 +8,8 @@ export default defineConfig({
 
     coverage: {
       all: true,
-      reporter: ['text', 'lcov'],
-      include: ['src/'],
+      reporter: ["text", "lcov"],
+      include: ["src/"],
     },
   },
-})
+});
